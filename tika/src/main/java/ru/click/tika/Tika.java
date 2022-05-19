0621ad78fe65e5bc2c752936078547c1e632cd12
Timur <t.nabiyev@click-group.ru>
2022-05-19 17:42:17 +0300

    Tika initial

---
+++ tika/src/main/java/ru/click/tika/Tika.java
@@ -0,0 +1,9 @@
+package ru.click.tika;
+
+public class Tika {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ tika/src/main/java/ru/click/tika/TikaService.java
@@ -0,0 +1,30 @@
+package ru.click.tika;
+
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class TikaService {
+
+    private final Tika tika = new Tika();
+
+    public void extractImages(InputStream stream) throws TikaException, IOException, SAXException {
+        Parser parser = new AutoDetectParser();
+        ContentHandler handler = new BodyContentHandler();
+        Metadata metadata = new Metadata();
+        ParseContext context = new ParseContext();
+
+        parser.parse(stream, handler, metadata, context);
+
+    }
+
+
+}
